d1561e95f7c06e3e13eda0df93f61a1225c6dea6
harry febiansyah <harryfebiansyah13@gmail.com>
2024-10-06 14:57:07 +0800

    jawaban ujian

--- quiz/ujian/soal3.js
+++ quiz/ujian/soal3.js
@@ -16,3 +16,5 @@ console.log(konversiMenit(124)); // 2:04
 console.log(konversiMenit(53)); // 0:53
 console.log(konversiMenit(88)); // 1:28
 console.log(konversiMenit(120)); // 2:00
+
+// will come back to this later
--- quiz/ujian/soal4.js
+++ quiz/ujian/soal4.js
@@ -40,3 +40,5 @@ console.log(xo('oxooxo')); // false
 console.log(xo('oxo')); // false
 console.log(xo('xxxooo')); // true
 console.log(xo('xoxooxxo')); // true
+
+// will come back to this later
